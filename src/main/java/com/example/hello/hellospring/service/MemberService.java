328e79cdd0cc905ad653287406822f4ddff108d1
essencedev9 <essencedev9@nntuple.com>
2021-07-23 20:20:42 +0900

    component scan

--- src/main/java/com/example/hello/hellospring/service/MemberService.java
+++ src/main/java/com/example/hello/hellospring/service/MemberService.java
@@ -2,14 +2,18 @@ package com.example.hello.hellospring.service;
 
 import com.example.hello.hellospring.domain.Member;
 import com.example.hello.hellospring.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService {
 
     private final MemberRepository memberRepository;
 
+    @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
