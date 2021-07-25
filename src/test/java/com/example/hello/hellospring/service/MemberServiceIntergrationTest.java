42df1cbaff66de18ad759170b07076fab86e7dd4
essencedev9 <essencedev9@nntuple.com>
2021-07-25 22:09:50 +0900

    Add Test - h2 with jdbc

---
+++ src/test/java/com/example/hello/hellospring/service/MemberServiceIntergrationTest.java
@@ -0,0 +1,54 @@
+package com.example.hello.hellospring.service;
+
+import com.example.hello.hellospring.domain.Member;
+import com.example.hello.hellospring.repository.MemberRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class MemberServiceIntergrationTest {
+
+    MemberService memberService;
+    MemberRepository memberRepository;
+
+    @Autowired
+    public MemberServiceIntergrationTest(MemberService memberService, MemberRepository memberRepository) {
+        this.memberService = memberService;
+        this.memberRepository = memberRepository;
+    }
+
+    @Test
+    void join() {
+        //given
+        Member member = new Member();
+        member.setName("spring");
+
+        //when
+        Long saveId = memberService.join(member);
+
+        //then
+        Member findMember = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    void duplicate_member_exception() {
+        //given
+        Member member1 = new Member();
+        member1.setName("spring1");
+
+        Member member2 = new Member();
+        member2.setName("spring1");
+
+        //when
+        memberService.join(member1);
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+    }
+}
