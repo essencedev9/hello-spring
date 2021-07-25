4a21bf7be785ce973e5a4c647b681948b1ac168d
essencedev9 <essencedev9@nntuple.com>
2021-07-25 22:32:27 +0900

    Add Repository - h2 with JdbcTemplate

--- src/main/java/com/example/hello/hellospring/SpringConfig.java
+++ src/main/java/com/example/hello/hellospring/SpringConfig.java
@@ -1,6 +1,7 @@
 package com.example.hello.hellospring;
 
 import com.example.hello.hellospring.repository.JdbcMemberRepository;
+import com.example.hello.hellospring.repository.JdbcTemplateMemberRepository;
 import com.example.hello.hellospring.repository.MemberRepository;
 import com.example.hello.hellospring.repository.MemoryMemberRepository;
 import com.example.hello.hellospring.service.MemberService;
@@ -26,6 +27,7 @@ public class SpringConfig {
     @Bean
     public MemberRepository memberRepository() {
         //return new MemoryMemberRepository();
-        return new JdbcMemberRepository(dataSource);
+        //return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 }
