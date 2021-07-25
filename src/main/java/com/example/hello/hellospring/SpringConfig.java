735becba07b892158c7425b446aab864696bec27
essencedev9 <essencedev9@nntuple.com>
2021-07-25 21:50:14 +0900

    Add Repository - h2 with jdbc

--- src/main/java/com/example/hello/hellospring/SpringConfig.java
+++ src/main/java/com/example/hello/hellospring/SpringConfig.java
@@ -1,13 +1,22 @@
 package com.example.hello.hellospring;
 
+import com.example.hello.hellospring.repository.JdbcMemberRepository;
 import com.example.hello.hellospring.repository.MemberRepository;
 import com.example.hello.hellospring.repository.MemoryMemberRepository;
 import com.example.hello.hellospring.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,6 +25,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        //return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
